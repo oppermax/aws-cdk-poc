749c5aef48da31030805280a5b0663923d49b78d
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-07-01 10:28:11 +0200

    first successful deployment

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
-module github.com/oppermax/replication-tester
+module infra
 
 go 1.16
 
-require github.com/aws/aws-cdk-go/awscdk v1.110.1-devpreview // indirect
+require (
+	github.com/aws/aws-cdk-go/awscdk v1.110.1-devpreview
+	github.com/aws/constructs-go/constructs/v3 v3.3.87
+	github.com/aws/jsii-runtime-go v1.30.0
+	github.com/stretchr/testify v1.7.0
+
+	// for testing
+	github.com/tidwall/gjson v1.7.4
+)
--- infra/infra.go
+++ infra/infra.go
@@ -2,81 +2,137 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk"
+	"github.com/aws/aws-cdk-go/awscdk/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/awss3"
 	"github.com/aws/constructs-go/constructs/v3"
 	"github.com/aws/jsii-runtime-go"
+)
+
+const (
+	sourceBucketName      = "max-test-replication-source-bucket"
+	destinationBucketName = "max-test-replication-destination-bucket"
 )
 
 type InfraStackProps struct {
 	awscdk.StackProps
 }
 
-func createReplicationRule(destinationBucket awss3.CfnBucket) awss3.CfnBucket_ReplicationRuleProperty {
+type replicationTester struct {
+	destinationBucket awss3.CfnBucket
+	sourceBucket      awss3.CfnBucket
+	replicatioRole    awsiam.Role
+	stack             awscdk.Stack
+}
+
+func (r *replicationTester) createReplicationRule() awss3.CfnBucket_ReplicationRuleProperty {
+	//dest := awss3.CfnBucket_ReplicationDestinationProperty{
+	//	Bucket:                   r.destinationBucket.BucketName(),
+	//	AccessControlTranslation: jsii.String("Destination"),
+	//	Metrics: awss3.CfnBucket_MetricsProperty{
+	//		Status: jsii.String("Enabled"),
+	//	},
+	//	ReplicationTime: awss3.CfnBucket_ReplicationTimeValueProperty{Minutes: jsii.Number(15)},
+	//}
+
+	dest := awss3.CfnBucket_ReplicationDestinationProperty{
+		Bucket: r.destinationBucket.AttrArn(),
+	}
+
 	return awss3.CfnBucket_ReplicationRuleProperty{
-		Destination:             awss3.CfnBucket_ReplicationDestinationProperty{
-			Bucket:                   destinationBucket.BucketName(),
-			AccessControlTranslation: jsii.String("Destination"),
-			Metrics:                  awss3.CfnBucket_MetricsProperty{
-				Status:         jsii.String("Enabled"),
-			},
-			ReplicationTime:          awss3.CfnBucket_ReplicationTimeValueProperty{Minutes: jsii.Number(15)},
-		},
+		Destination:             dest,
 		Status:                  jsii.String("Enabled"),
-		DeleteMarkerReplication: awss3.CfnBucket_DeleteMarkerReplicationProperty{Status: jsii.String("Enabled")},
 		Id:                      jsii.String("max-test-replication-rule"),
 	}
 }
 
-func NewReplicationSourceStack(scope constructs.Construct, id string, props *InfraStackProps, destinationBucket awss3.CfnBucket) awscdk.Stack {
+func (r *replicationTester) CreateSourceComponents() {
+
+	r.sourceBucket = awss3.NewCfnBucket(r.stack, jsii.String("MaxTestReplicationSourceBucket"), &awss3.CfnBucketProps{
+		BucketName:              jsii.String(sourceBucketName),
+		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{Status: jsii.String("Enabled")},
+	})
+
+	r.sourceBucket.SetReplicationConfiguration(awss3.CfnBucket_ReplicationConfigurationProperty{
+		Role:  r.replicatioRole.RoleArn(),
+		Rules: []awss3.CfnBucket_ReplicationRuleProperty{r.createReplicationRule()},
+	})
+
+}
+
+func (r *replicationTester) CreateDestinationComponents() {
+
+	r.destinationBucket = awss3.NewCfnBucket(r.stack, jsii.String("MaxTestReplicationDestinationBucket"), &awss3.CfnBucketProps{
+		BucketName: jsii.String(destinationBucketName),
+		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{Status: jsii.String("Enabled")},
+	})
+
+}
+
+func (r *replicationTester) addPoliciesToRole() {
+	policy := awsiam.NewPolicyDocument(nil)
+
+	policy.AddStatements(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Actions:   &[]*string{jsii.String("s3:ReplicateObject"), jsii.String("s3:ReplicateDelete"), jsii.String("s3:ReplicateTags")},
+		Effect:    awsiam.Effect_ALLOW,
+		Resources: &[]*string{r.destinationBucket.AttrArn()},
+	}))
+
+	policy.AddStatements(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Actions:   &[]*string{jsii.String("s3:GetReplicationConfiguration"), jsii.String("s3:ListBucket")},
+		Effect:    awsiam.Effect_ALLOW,
+		Resources: &[]*string{r.sourceBucket.AttrArn()},
+	}))
+
+	policy.AddStatements(awsiam.NewPolicyStatement(&awsiam.PolicyStatementProps{
+		Actions:   &[]*string{jsii.String("s3:GetObjectVersionForReplication"), jsii.String("s3:GetObjectVersionAcl"), jsii.String("s3:GetObjectVersionTagging")},
+		Effect:    awsiam.Effect_ALLOW,
+		Resources: &[]*string{jsii.String(*r.destinationBucket.AttrArn() + "/*")},
+	}))
+
+	//policies := &map[string]awsiam.PolicyDocument{"one": policy}
+
+	r.replicatioRole.AttachInlinePolicy(awsiam.NewPolicy(r.stack, jsii.String("max-test-replication-policy"), &awsiam.PolicyProps{
+		Document: policy,
+	}))
+}
+
+func (r *replicationTester) CreateIamRoleComponents() {
+
+	r.replicatioRole = awsiam.NewRole(r.stack, jsii.String("max-test-replication-role"), &awsiam.RoleProps{
+		AssumedBy:      awsiam.NewServicePrincipal(jsii.String("s3.amazonaws.com"), nil),
+		Description:    jsii.String("test role for replication oppermax"),
+		InlinePolicies: nil,
+		RoleName:       jsii.String("max-test-replication-role"),
+	})
+
+}
+
+func (r *replicationTester) NewReplicationTestStack(scope constructs.Construct, id string, props *InfraStackProps) awscdk.Stack {
 	var sprops awscdk.StackProps
 	if props != nil {
 		sprops = props.StackProps
 	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
+	r.stack = awscdk.NewStack(scope, &id, &sprops)
 
 	// The code that defines your stack goes here
 
-	bucket := awss3.NewCfnBucket(stack, jsii.String("MaxTestReplicationSource"), &awss3.CfnBucketProps{
-		BucketName:                       jsii.String("MaxTestReplicationSource"),
-	})
+	r.CreateDestinationComponents()
+	r.CreateIamRoleComponents()
+	r.CreateSourceComponents()
+	r.addPoliciesToRole()
 
-	bucket.SetReplicationConfiguration(createReplicationRule(destinationBucket))
-
-	return stack
+	return r.stack
 }
 
-
-func NewReplicationDestinationStack(scope constructs.Construct, id string, props *InfraStackProps) (awscdk.Stack, awss3.CfnBucket) {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-
-	// The code that defines your stack goes here
-
-	bucket := awss3.NewCfnBucket(stack, jsii.String("MaxTestReplicationDestination"), &awss3.CfnBucketProps{
-		BucketName:                       jsii.String("MaxTestReplicationDestination"),
-	})
-
-
-
-	return stack, bucket
-}
 func main() {
 	app := awscdk.NewApp(nil)
 
-	_, destBucket  := NewReplicationDestinationStack(app, "MaxTestReplicationSourceStack", &InfraStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	})
+	r := replicationTester{}
 
-	NewReplicationSourceStack(app, "MaxTestReplicationDestinationStack", &InfraStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	}, destBucket)
+	r.NewReplicationTestStack(app, "max-test-replication-stack", &InfraStackProps{awscdk.StackProps{
+		Env:       env(),
+		StackName: jsii.String("max-test-replication-stack"),
+	}})
 
 	app.Synth(nil)
 }
