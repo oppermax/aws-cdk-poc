2df0f08619567d86d85029eb362f59ac19567a7e
Max Opperman <55201171+oppermax@users.noreply.github.com>
2021-07-02 16:24:23 +0200

    add cw alarm

--- infra/infra.go
+++ infra/infra.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/aws/aws-cdk-go/awscdk"
+	"github.com/aws/aws-cdk-go/awscdk/awscloudwatch"
 	"github.com/aws/aws-cdk-go/awscdk/awsiam"
 	"github.com/aws/aws-cdk-go/awscdk/awss3"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -42,7 +43,9 @@ type replicationTester struct {
 	destinationBucket awss3.CfnBucket
 	sourceBucket      awss3.CfnBucket
 	replicatioRole    awsiam.Role
-	stack             awscdk.Stack
+	replcationRule    awss3.CfnBucket_ReplicationRuleProperty
+	replicationStack  awscdk.Stack
+	monitoringStack   awscdk.Stack
 }
 
 func (r *replicationTester) createReplicationRule() awss3.CfnBucket_ReplicationRuleProperty {
@@ -65,7 +68,7 @@ func (r *replicationTester) createReplicationRule() awss3.CfnBucket_ReplicationR
 		Status:      jsii.String("Enabled"),
 		Id:          jsii.String("max-test-replication-rule"),
 		Filter: awss3.CfnBucket_ReplicationRuleFilterProperty{
-			Prefix:    jsii.String(""),
+			Prefix: jsii.String(""),
 		},
 		DeleteMarkerReplication: awss3.CfnBucket_DeleteMarkerReplicationProperty{Status: jsii.String("Disabled")},
 		Priority:                jsii.Number(1),
@@ -74,12 +77,14 @@ func (r *replicationTester) createReplicationRule() awss3.CfnBucket_ReplicationR
 
 func (r *replicationTester) CreateSourceComponents() {
 
-	r.sourceBucket = awss3.NewCfnBucket(r.stack, jsii.String("MaxTestReplicationSourceBucket"), &awss3.CfnBucketProps{
+	r.replcationRule = r.createReplicationRule()
+
+	r.sourceBucket = awss3.NewCfnBucket(r.replicationStack, jsii.String("MaxTestReplicationSourceBucket"), &awss3.CfnBucketProps{
 		BucketName:              jsii.String(sourceBucketName),
 		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{Status: jsii.String("Enabled")},
 		ReplicationConfiguration: awss3.CfnBucket_ReplicationConfigurationProperty{
 			Role:  r.replicatioRole.RoleArn(),
-			Rules: []awss3.CfnBucket_ReplicationRuleProperty{r.createReplicationRule()},
+			Rules: []awss3.CfnBucket_ReplicationRuleProperty{r.replcationRule},
 		},
 	})
 
@@ -87,7 +92,7 @@ func (r *replicationTester) CreateSourceComponents() {
 
 func (r *replicationTester) CreateDestinationComponents() {
 
-	r.destinationBucket = awss3.NewCfnBucket(r.stack, jsii.String("MaxTestReplicationDestinationBucket"), &awss3.CfnBucketProps{
+	r.destinationBucket = awss3.NewCfnBucket(r.replicationStack, jsii.String("MaxTestReplicationDestinationBucket"), &awss3.CfnBucketProps{
 		BucketName:              jsii.String(destinationBucketName),
 		VersioningConfiguration: awss3.CfnBucket_VersioningConfigurationProperty{Status: jsii.String("Enabled")},
 	})
@@ -121,7 +126,7 @@ func (r *replicationTester) addPoliciesToReplicationRole() {
 		Resources: &[]*string{jsii.String(*r.destinationBucket.AttrArn() + "/*")},
 	}))
 
-	r.replicatioRole.AttachInlinePolicy(awsiam.NewPolicy(r.stack, jsii.String("max-test-replication-policy"), &awsiam.PolicyProps{
+	r.replicatioRole.AttachInlinePolicy(awsiam.NewPolicy(r.replicationStack, jsii.String("max-test-replication-policy"), &awsiam.PolicyProps{
 		Document: policy,
 	}))
 }
@@ -147,20 +152,19 @@ func (r *replicationTester) addPoliciesToReplicationRole() {
 //		Resources: &[]*string{jsii.String(*r.destinationBucket.AttrArn() + "/*")},
 //	}))
 //
-//	r.replicatioRole.AttachInlinePolicy(awsiam.NewPolicy(r.stack, jsii.String("max-test-replication-policy"), &awsiam.PolicyProps{
+//	r.replicatioRole.AttachInlinePolicy(awsiam.NewPolicy(r.replicationStack, jsii.String("max-test-replication-policy"), &awsiam.PolicyProps{
 //		Document: policy,
 //	}))
 //}
 
-
 func (r *replicationTester) CreateIamRoleComponents() {
 
-	r.replicatioRole = awsiam.NewRole(r.stack, jsii.String("max-test-replication-role"), &awsiam.RoleProps{
+	r.replicatioRole = awsiam.NewRole(r.replicationStack, jsii.String("max-test-replication-role"), &awsiam.RoleProps{
 		AssumedBy:      awsiam.NewServicePrincipal(jsii.String("s3.amazonaws.com"), nil),
 		Description:    jsii.String("test role for replication oppermax"),
 		InlinePolicies: nil,
 		RoleName:       jsii.String("max-test-replication-role"),
-		Path: jsii.String("/"),
+		Path:           jsii.String("/"),
 	})
 
 }
@@ -170,16 +174,68 @@ func (r *replicationTester) NewReplicationTestStack(scope constructs.Construct, 
 	if props != nil {
 		sprops = props.StackProps
 	}
-	r.stack = awscdk.NewStack(scope, &id, &sprops)
+	r.replicationStack = awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	// The code that defines your replicationStack goes here
 
 	r.CreateDestinationComponents()
 	r.CreateIamRoleComponents()
 	r.CreateSourceComponents()
 	r.addPoliciesToReplicationRole()
 
-	return r.stack
+	return r.replicationStack
+}
+
+func (r *replicationTester) NewReplicationMonitoringTestStack(scope constructs.Construct, id string, props *InfraStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	// The code that defines your replicationStack goes here
+	awscloudwatch.NewCfnAnomalyDetector(stack, jsii.String("foo"), &awscloudwatch.CfnAnomalyDetectorProps{
+		MetricName: jsii.String("OperationsPendingReplication"),
+		Namespace:  jsii.String("AWS/S3"),
+		Stat:       jsii.String("Average"),
+	})
+
+	awscloudwatch.NewCfnAlarm(stack, jsii.String("foo"), &awscloudwatch.CfnAlarmProps{
+		ComparisonOperator: jsii.String("GreaterThanUpperThreshold"),
+		EvaluationPeriods:  jsii.Number(2),
+		ActionsEnabled:     jsii.Bool(true),
+		AlarmDescription:   jsii.String("the s3 bucket is on fire"),
+		AlarmName:          jsii.String("max-test-replication-alarm"),
+		DatapointsToAlarm:  jsii.Number(2),
+		ThresholdMetricId:  jsii.String("ad1"),
+		TreatMissingData:   jsii.String("ignore"),
+		Metrics: []awscloudwatch.CfnAlarm_MetricDataQueryProperty{{
+			Id: jsii.String("ad1"),
+			Expression: jsii.String("ANOMALY_DETECTION_BAND(m1, 16)"),
+		},{
+			Id:         jsii.String("m1"),
+			MetricStat: awscloudwatch.CfnAlarm_MetricStatProperty{
+				Stat: jsii.String("Average"),
+				Period: jsii.Number(300),
+				Metric: awscloudwatch.CfnAlarm_MetricProperty{
+					Dimensions: []awscloudwatch.CfnAlarm_DimensionProperty{{
+						Name:  jsii.String("SourceBucket"),
+						Value: r.sourceBucket.BucketName(),
+					}, {
+						Name:  jsii.String("DestinationBucket"),
+						Value: r.destinationBucket.BucketName(),
+					}, {
+						Name:  jsii.String("RuleId"),
+						Value: r.replcationRule.Id,
+					}},
+					MetricName: jsii.String("OperationsPendingReplication"),
+					Namespace:  jsii.String("AWS/S3"),
+				},
+			},
+		}},
+	})
+
+	return stack
 }
 
 func main() {
@@ -187,32 +243,32 @@ func main() {
 
 	r := replicationTester{}
 
-	r.NewReplicationTestStack(app, "max-test-replication-stack", &InfraStackProps{awscdk.StackProps{
+	r.NewReplicationTestStack(app, "max-test-replication-replicationStack", &InfraStackProps{awscdk.StackProps{
 		Env:       env(),
-		StackName: jsii.String("max-test-replication-stack"),
+		StackName: jsii.String("max-test-replication-replicationStack"),
 	}})
 
 	app.Synth(nil)
 }
 
-// env determines the AWS environment (account+region) in which our stack is to
+// env determines the AWS environment (account+region) in which our replicationStack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
+	// If unspecified, this replicationStack will be "environment-agnostic".
 	// Account/Region-dependent features and context lookups will not work, but a
 	// single synthesized template can be deployed anywhere.
 	//---------------------------------------------------------------------------
 	return nil
 
 	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
+	// the replicationStack to. This is the recommendation for production stacks.
 	//---------------------------------------------------------------------------
 	// return &awscdk.Environment{
 	//  Account: jsii.String("123456789012"),
 	//  Region:  jsii.String("us-east-1"),
 	// }
 
-	// Uncomment to specialize this stack for the AWS Account and Region that are
+	// Uncomment to specialize this replicationStack for the AWS Account and Region that are
 	// implied by the current CLI configuration. This is recommended for dev
 	// stacks.
 	//---------------------------------------------------------------------------
